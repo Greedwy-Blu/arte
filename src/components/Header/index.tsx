8e285b52de855f3e0601bc5db438bccf7b2c2fdb
Greedwy-Blu <greedwyblu@gmail.com>
2023-02-17 19:14:48 -0300

    update

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,7 +4,7 @@ function HeaderBody(){
 
       <div className="flex justify-around items-center">
         <div className="h-[300px] w-[300px] bg-laranja-1 "></div>
-        <div className="h-[500px] w-[500px]  bg-laranja-1  "></div>
+        <div className="h-[500px] w-[500px]  bg-vermelho-3 rounded-tl-[240px]  rounded-tr-[240px]  outline outline-1  outline-branco-1"></div>
         <div className="h-[300px] w-[300px]  bg-laranja-1  "></div>
       </div>
 
