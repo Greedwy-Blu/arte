1f82cc8062ff2988e9b40ffab4e80c770de9bea8
Greedwy-Blu <greedwyblu@gmail.com>
2023-02-17 17:18:00 -0300

    colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,25 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    colors:{
+      'branco-1': '#F2DDD0',
+      'branco-2': '#F2F2F2',
+      'preto-1': '#0D0D0D',
+      'preto-2': '#172526',
+      'cinza-1': '#594C45',
+      'cinza-2': '#8C7F77',
+      'cinza-3': '#A69185',
+      'cinza-4': '#594F46',
+      'cinza-5': '#594F46',
+      'cinza-6': '#8C837B',
+      'cinza-7': '#A6A09C',
+      'cinza-8': '#A69F92',
+      'vermelho-1': '#592323',
+      'vermelho-2': '#8C3B42',
+      'vermelho-3': '#73172F',
+      'vermelho-4': '#400A05',
+
+    },
     extend: {},
   },
   plugins: [],
