abc66bc6cfa4ab4a5aa1a148a3a603ddeca72ee7
Greedwy-Blu <greedwyblu@gmail.com>
2023-02-18 16:28:14 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import { useState } from 'react'
 import TextHeader from './components/TextHeader'
 import HeaderBody from './components/Header'
+import ArtNoiteEstralada from './components/ArtNoiteEstralada'
+import ArtGirl from './components/ArtGirl'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <main>
-     <TextHeader/>
-      <HeaderBody/>
+    <TextHeader/>
+    <HeaderBody/>
+    <ArtNoiteEstralada/>
+    <ArtGirl/>
 
     </main>
   )
