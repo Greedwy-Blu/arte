aa5049e0cc3855a7c50a0db036833a920d949d8f
Greedwy-Blu <greedwyblu@gmail.com>
2023-02-18 21:56:02 -0300

    update

--- src/components/ArtNoiteEstralada/index.tsx
+++ src/components/ArtNoiteEstralada/index.tsx
@@ -4,7 +4,7 @@ function ArtNoiteEstralada(){
   return(
 <section className="bg-[#033649] h-[800px] w-full mt-32 ">
 
-    <div className="flex justify-center translate-y-20">
+    <div className="flex justify-center translate-y-20 -translate-x-24">
     <a className="text-5xl text-[#F2C36B]">NOITE ESTRELADA</a>
     </div>
 
@@ -13,10 +13,13 @@ function ArtNoiteEstralada(){
     <img src={imgNoiteEstrelada} alt="" className='h-[500px] w-[600px]' />
   </div>
 
-  <div className=" overflow-hidden h-[400px] w-[300px] rounded-tl-[137px] rounded-tr-[137px] rounded-br-[154px] rounded-bl-[154px]  bg-[#252617] ">
-          <img src={imgCampo} className='h-[400px] w-[300px]  '/>
-        </div>
+  <div className=" overflow-hidden h-[400px] w-[300px] rounded-tl-[137px] rounded-tr-[137px] rounded-br-[154px] rounded-bl-[154px]  bg-[#252617] translate-x-24">
 
+          <img src={imgCampo} className='h-[400px] w-[300px]  '/>
+
+
+        </div>
+        <div className='mt-[500px] -translate-y-20 -translate-x-24'><a className=' text-[#F2C36B] font-bold'>CAMPO DE TRIGO COM CORVOS</a></div>
         </div>
 
 </section>
