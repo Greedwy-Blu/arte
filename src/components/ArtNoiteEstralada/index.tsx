e37824031aea90262f8a599257c558dac5be64b7
Greedwy-Blu <greedwyblu@gmail.com>
2023-02-18 21:46:34 -0300

    update

--- src/components/ArtNoiteEstralada/index.tsx
+++ src/components/ArtNoiteEstralada/index.tsx
@@ -1,16 +1,24 @@
 import imgNoiteEstrelada from '../../assets/8deaf0b3-2477-4a93-a246-f69c5dbb188e.jpeg'
-
+import imgCampo from  '../../assets/Campo de trigo com corvos, 1890 ( Vincent Van Gogh.jpeg'
 function ArtNoiteEstralada(){
   return(
-<section className="bg-[#033649] h-[800px] w-full mt-32">
+<section className="bg-[#033649] h-[800px] w-full mt-32 ">
 
     <div className="flex justify-center translate-y-20">
     <a className="text-5xl text-[#F2C36B]">NOITE ESTRELADA</a>
     </div>
 
-  <div className='flex justify-center mt-32'>
+<div className='flex justify-between'>
+  <div className=' mt-32'>
     <img src={imgNoiteEstrelada} alt="" className='h-[500px] w-[600px]' />
   </div>
+
+  <div className=" overflow-hidden h-[400px] w-[300px] rounded-tl-[137px] rounded-tr-[137px] rounded-br-[154px] rounded-bl-[154px]  bg-[#252617] ">
+          <img src={imgCampo} className='h-[400px] w-[300px]  '/>
+        </div>
+
+        </div>
+
 </section>
   )
 }
